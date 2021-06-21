0c55c20911ce0e753214452e759757e1710cee07
ngoctuan1 <72248343+ngoctuan1@users.noreply.github.com>
2021-06-21 13:41:13 +0700

    Update Constants.java

--- TestChatBot/src/main/java/com/example/demo/util/Constants.java
+++ TestChatBot/src/main/java/com/example/demo/util/Constants.java
@@ -3,5 +3,5 @@ package com.example.demo.util;
 public  class Constants {
 	public static String USER_NAME = "tuanngoc0060@gmail.com";
 	public static String PASS  = "tuanngoc0060@@@@";
-	public static String URL_SEND_MESSAGE = "https://www.messenger.com/t/";
+	public static String URL_SEND_MESSAGE = "https://mbasic.facebook.com/messages/read/?fbid=";
 }
